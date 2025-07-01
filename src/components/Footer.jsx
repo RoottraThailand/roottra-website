7cd77d4f4a897c0f2f83a167661d860264e6128f
RoottraThailand <roottra.business@gmail.com>
2025-07-01 17:58:31 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center">
           <p className="text-gray-500">
-            © {new Date().getFullYear()} Roottra. All rights reserved.
+            © {new Date().getFullYear()} Root Tra. All rights reserved.
           </p>
         </div>
       </div>
