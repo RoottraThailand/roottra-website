61ac946004c796433d74b7b377f02bca9a8eb164
RoottraThailand <roottra.business@gmail.com>
2025-10-02 15:58:47 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Hero from './components/Hero.jsx';
 import Mission from './components/Mission.jsx';
+import Targets from './components/Targets.jsx';   // NEW
 import Vision from './components/Vision.jsx';
 import WhatWeDo from './components/WhatWeDo.jsx';
+import LandscapeSection from './components/LandscapeSection.jsx';
 import Team from './components/Team.jsx';
+import OurStory from './components/OurStory.jsx';
 import OurWork from './components/OurWork.jsx';
 import Footer from './components/Footer.jsx';
 
@@ -11,9 +14,12 @@ function App() {
     <div className="bg-black text-white min-h-screen">
       <Hero />
       <Mission />
+      <Targets />      {/* NEW SECTION */}
       <Vision />
       <WhatWeDo />
+      <LandscapeSection />
       <Team />
+      <OurStory />
       <OurWork />
       <Footer />
     </div>
